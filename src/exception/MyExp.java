bb53dd021640badd9d90e6f367bf439985d058fb
laura León <isabelaleon8@gmail.com>
2020-05-22 14:01:26 -0500

    creo repositorio

---
+++ src/exception/MyExp.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class MyExp extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public MyExp(String message) {
+		super(message);
+		
+	}
+
+}
---
+++ src/view/Main.java
@@ -0,0 +1,43 @@
+package view;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PApplet.main("View.Main");
+		
+		
+
+	}
+	public void settings() {
+		size(800,500);
+		
+		
+	}
+	
+	
+	public void setup() {
+		background(131,112,199);
+		
+	}
+	
+	public void draw() {
+		background(131,112,199);
+		
+		
+	}
+	public void mousePressed() {
+		
+		
+	}
+	
+	public void keyPressed() {
+			
+		
+	
+	}
+	
+
+}
